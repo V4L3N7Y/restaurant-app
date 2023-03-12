76075ebdd2f3f562a245c4b852e3f43507a6803d
V4L3N7Y <valimihai154@yahoo.com>
2023-03-12 12:49:10 +0200

    'Go To' button opens google maps

--- components/RestaurantsApi.js
+++ components/RestaurantsApi.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Dimensions, TouchableOpacity, Linking } from 'react-native';
 
 
-const API_KEY = 'secret';
+const API_KEY = 'AIzaSyAFyA9ta0evqTROKT8E63tUUBwhIrk6Uok';
 const LATITUDE = 44.449468; // example latitude
 const LONGITUDE = 26.160961; // example longitude
 const RADIUS = 117000; // Romania radius in meters
@@ -61,6 +61,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
   },
+  rating: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#999',
+    marginBottom: 8,
+  },
 });
 
 export default function RestaurantsApi() {
@@ -84,31 +90,50 @@ export default function RestaurantsApi() {
     return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${API_KEY}`;
   };
 
+  
+  const openMaps = (lat, lng) => {
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+    console.log('Maps URL:', url);
+    Linking.openURL(url).then(() => {
+      console.log('Opened Maps successfully');
+    }).catch((err) => {
+      console.error('Failed to open Maps:', err);
+    });
+  };
+  
+
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <View style={styles.restaurantColumn}>
+    <ScrollView>
+      <View style={styles.restaurantColumn}>
         {restaurants.map((restaurant, index) => (
-  <View key={index} style={styles.restaurantBox}>
-    {restaurant.photos && restaurant.photos[0] && (
-      <Image
-        style={styles.restaurantImage}
-        source={{
-          uri: getPhotoUrl(restaurant.photos[0].photo_reference),
-        }}
-      />
-    )}
-    <Text style={styles.restaurantName}>{restaurant.name}</Text>
-    <Text style={styles.restaurantAddress}>{restaurant.vicinity}</Text>
-    <TouchableOpacity onPress={() => alert(`Button pressed for ${restaurant.name}`)}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>Go To!</Text>
+          <View key={index} style={styles.restaurantBox}>
+            {restaurant.photos && restaurant.photos[0] && (
+              <Image
+                style={styles.restaurantImage}
+                source={{
+                  uri: getPhotoUrl(restaurant.photos[0].photo_reference),
+                }}
+              />
+            )}
+            <Text style={styles.restaurantName}>{restaurant.name}</Text>
+            <Text style={styles.restaurantAddress}>{restaurant.vicinity}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}>
+              <Image
+                source={require('../images/star.png')}
+                style={{ width: 24, height: 24, marginRight: 4 }}
+              />
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Rating: {restaurant.rating} out of 5</Text>
+            </View>
+            <TouchableOpacity onPress={() => openMaps(restaurant)}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Go To!</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        ))}
       </View>
-    </TouchableOpacity>
+    </ScrollView>
   </View>
-))}
-        </View>
-      </ScrollView>
-    </View>
   );
 }
