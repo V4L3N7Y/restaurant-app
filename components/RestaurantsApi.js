3bd3a2b8a888f49cdca1599081b82ba6ff98be56
V4L3N7Y <valimihai154@yahoo.com>
2023-03-14 09:47:28 +0200

    renders reviews from every restaurant

--- components/RestaurantsApi.js
+++ components/RestaurantsApi.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, ScrollView, Image, Dimensions, TouchableOpacity
 
 
 const API_KEY = 'AIzaSyAFyA9ta0evqTROKT8E63tUUBwhIrk6Uok';
-const LATITUDE = 44.449468; // example latitude
-const LONGITUDE = 26.160961; // example longitude
-const RADIUS = 117000; // Romania radius in meters
+const LATITUDE = 44.4379853; // example latitude
+const LONGITUDE = 25.9545531; // example longitude
+const RADIUS = 27000; // Romania radius in meters
 const { width } = Dimensions.get('window');
 const CARD_WIDTH = width - 32;
 
@@ -16,10 +16,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#F5FCFF',
+    
   },
   restaurantBox: {
     width: CARD_WIDTH,
     borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
     padding: 16,
     marginBottom: 16,
     backgroundColor: '#fff',
@@ -50,7 +54,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     height: 50,
-    maxWidth: 100,
+    minWidth: '90%',
     backgroundColor: '#007AFF',
     borderRadius: 8,
     padding: 8,
@@ -71,7 +75,9 @@ const styles = StyleSheet.create({
 
 export default function RestaurantsApi() {
   const [restaurants, setRestaurants] = useState([]);
-
+  const [nextPageToken, setNextPageToken] = useState('');
+  
+  ///render the restaurants on screen
   useEffect(() => {
     fetch(
       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LATITUDE},${LONGITUDE}&radius=${RADIUS}&type=restaurant&key=${API_KEY}`
@@ -79,18 +85,41 @@ export default function RestaurantsApi() {
       .then((response) => response.json())
       .then((data) => {
         setRestaurants(data.results);
-        // console.log(data.results)
+        setNextPageToken(data.next_page_token); 
+        const promises = data.results.map((restaurant) => {
+          return fetch(
+            `https://maps.googleapis.com/maps/api/place/details/json?place_id=${restaurant.place_id}&fields=name,formatted_address,photo,rating,opening_hours,website,reviews&key=${API_KEY}`
+          )
+            .then((response) => response.json())
+            .then((detailsData) => {
+              // Add the reviews to the restaurant object
+              restaurant.reviews = detailsData.result.reviews;
+              console.log(restaurant.reviews)
+              return restaurant;
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        });
+  
+        // Wait for all API calls to finish and set the state with the updated restaurants
+        Promise.all(promises).then((restaurantsWithReviews) => {
+          setRestaurants(restaurantsWithReviews);
+        });
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
+
+  ///show photos from the restaurants
   const getPhotoUrl = (photoRef) => {
     return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${API_KEY}`;
   };
 
-  
+
+  /// open the maps 
   const openMaps = (places) => {
     const url = `https://www.google.com/maps/dir/?api=1&destination=${places.geometry.location.lat},${places.geometry.location.lng}`;
     console.log('Maps URL:', url);
@@ -100,6 +129,23 @@ export default function RestaurantsApi() {
       console.error('Failed to open Maps:', err);
     });
   };
+
+/// loading more restaurants 
+  const loadMoreRestaurants = () => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${LATITUDE},${LONGITUDE}&radius=${RADIUS}&type=restaurant&key=${API_KEY}&pagetoken=${nextPageToken}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRestaurants((prevRestaurants) => [...prevRestaurants, ...data.results]); // add the new results to the existing array of restaurants
+        setNextPageToken(data.next_page_token); // update the next page token
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  
   
 
   return (
@@ -116,9 +162,17 @@ export default function RestaurantsApi() {
                 }}
               />
             )}
+            {nextPageToken && (
+      <TouchableOpacity title="Load more" onPress={() => loadMoreRestaurants} ></TouchableOpacity>
+       )}
             <Text style={styles.restaurantName}>{restaurant.name}</Text>
             <Text style={styles.restaurantAddress}>{restaurant.vicinity}</Text>
             <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}>
+              {restaurant.reviews && restaurant.reviews.map((review) => (
+              <View key={review.time}>
+              <Text>{review.rating} stars - {review.text}</Text>
+               </View>
+              ))}
               <Image
                 source={require('../images/star.png')}
                 style={{ width: 24, height: 24, marginRight: 4 }}
@@ -134,6 +188,7 @@ export default function RestaurantsApi() {
         ))}
       </View>
     </ScrollView>
+    
   </View>
   );
 }
